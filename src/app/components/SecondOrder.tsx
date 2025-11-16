1cfb6b51216b45bcb05bfbac157b431770f024ab
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2025-11-17 00:13:45 +0100

    commit 17/11/25

--- src/app/components/SecondOrder.tsx
+++ src/app/components/SecondOrder.tsx
@@ -4,20 +4,90 @@ import React from 'react'
 const SecondOrder = () => {
   return (
     <section id='second_order' className='relative flex flex-col lg:min-h-auto md:min-h-auto 2xl:mb-10 md:mb-20 sm:mb-6 max-sm:mb-10 lg:mt-0'>
-      <div className='grid lg:grid-cols-12 2xl:gap-8 lg:gap-x-8 lg:gap-y-2 md:gap-x-8 sm:gap-x-8 md:grid-cols-6 sm:grid-cols-6 max-sm:grid-cols-2 lg:mt-0 lg:pr-[134px] lg:pl-[134px] md:pr-[34px] md:pl-[34px] sm:pl-[34px] sm:pr-[34px] max-sm:pl-[34px] max-sm:pr-[34px] md:w-auto md:mx-0 sm:w-full sm:mx-auto max-sm:mx-auto max-sm:justify-center max-sm:w-full max-sm:px-4 '>
+      <div className='grid lg:grid-cols-12 2xl:gap-8 lg:gap-x-4 lg:gap-y-2 md:gap-x-8 sm:gap-x-8 md:grid-cols-6 sm:grid-cols-6 max-sm:grid-cols-2 lg:mt-0 lg:pr-[134px] lg:pl-0 md:pr-[34px] md:pl-[34px] sm:pl-[34px] sm:pr-[34px] max-sm:pl-[34px] max-sm:pr-[34px] md:w-auto md:mx-0 sm:w-full sm:mx-auto max-sm:mx-auto max-sm:justify-center max-sm:w-full max-sm:px-4 '>
         <div className="relative lg:col-start-1 lg:col-span-6 ">
 
           <div>
             <Image
               src="/images/Photos.svg"
               alt="pot title"
-              width={568}
+              width={663}
               height={460}
-              className='object-cover lg:object-center 2xl:w-[100%] 2xl:h-[100%] lg:w-[100%] lg:h-[100%] md:mb-7.5 max-sm:mb-6'
+              className='object-cover md:mb-7.5 max-sm:mb-6'
             />
           </div>
 
         </div>
+
+
+        <div className="relative mt-[186px] lg:col-start-7 lg:col-span-3 md:col-start-1 md:col-span-2 sm:col-start-1 sm:col-span-2 max-sm:col-start-1 max-sm:col-span-2 md:block md:mb-0 sm:mb-12 max-sm:mb-4">
+          <div className='flex w-[290px] gap-2.5 items-start mb-10'>
+            <Image
+              src="/images/Dot.svg"
+              alt="pot title"
+              width={7}
+              height={7}
+              className='object-cover lg:w-[8px] lg:h-[8px] mt-7'
+            />
+            <div>
+              <p className="mb-2.5 font-bold font-sans text-[#333333] leading-[1.5] text-[36px]">
+                Easy assembly
+              </p>
+              <p className="font-sans text-[#4F4F4F] leading-[1.5] text-base">
+                Pull the draw string to assemble!
+              </p>
+            </div>
+          </div>
+
+
+
+          <div className='flex w-[290px] gap-2.5 items-start mb-15'>
+            <Image
+              src="/images/Dot.svg"
+              alt="pot title"
+              width={7}
+              height={7}
+              className='object-cover lg:w-[8px] lg:h-[8px] mt-6'
+            />
+            <div>
+              <p className="mb-2.5 font-bold font-sans text-[#333333] leading-[1.5] text-[36px]">
+                Self-watering
+              </p>
+              <p className="font-sans text-[#4F4F4F] leading-[1.5] text-base">
+                Add water to POTR and watch your plant drink!
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full flex flex-col items-start lg:mt-[20px] md:mt-[50px] sm:mt-[50px] max-sm:mt-[20px] max-sm:pb-5 lg:pr-0 md:pr-[90px] z-10">
+            <button className="lg:w-[238px] md:w-[240px] sm:w-[240px] max-sm:w-full h-[48px] border border-[#03839E] hover:bg-[#03839E] tracking-[0] font-sans cursor-pointer font-semibold text-[14px] flex items-center justify-center gap-2 transition-colors duration-300">
+              BUY NOW  <span className=" ml-2">⟶</span>
+            </button>
+          </div>
+
+        </div>
+
+        <div className="relative mt-[186px] lg:col-start-10 lg:col-span-3 md:col-start-1 md:col-span-2 sm:col-start-1 sm:col-span-2 max-sm:col-start-1 max-sm:col-span-2 md:block md:mb-0 sm:mb-12 max-sm:mb-4">
+          <div className='flex w-[390px] gap-2.5 items-start'>
+            <Image
+              src="/images/Dot.svg"
+              alt="pot title"
+              width={7}
+              height={7}
+              className='object-cover lg:w-[8px] lg:h-[8px] mt-6'
+            />
+            <div>
+              <p className="mb-2.5 font-bold font-sans text-[#333333] leading-[1.5] text-[36px]">
+                Flat pack postage
+              </p>
+              <p className="font-sans text-[#4F4F4F] leading-[1.5] text-base">
+                POTR pots arrive flat packed in an envelope!
+              </p>
+            </div>
+          </div>
+        </div>
+
+
 
       </div>
     </section>
