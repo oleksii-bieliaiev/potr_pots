68f1f2265c3dec1acc232e7f537b37c03fa3661d
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2025-11-30 23:57:41 +0100

    commit 30/11/25

--- src/app/components/Questions.tsx
+++ src/app/components/Questions.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import ContactForm from './ContactForm'
+import Image from 'next/image'
 
 const Questions = () => {
   return (
     <section id='second_order' className='relative flex flex-col lg:min-h-auto md:min-h-auto 2xl:mb-10 md:mb-20 sm:mb-6 md:mt-0 sm:mt-25 max-sm:mt-25 max-sm:mb-10 lg:mt-0'>
-       <div className='grid lg:grid-cols-12 2xl:gap-8 lg:gap-x-8 lg:gap-y-2 md:grid-cols-6 sm:grid-cols-6 max-sm:grid-cols-2 lg:pr-[134px] lg:pl-[134px] md:pr-[34px] md:pl-[34px] sm:pl-[34px] max-sm:pl-0 sm:pr-[34px] max-sm:pr-0  md:w-auto md:mx-0 sm:w-full sm:mx-auto max-sm:w-full max-sm:mx-auto  '>
+      <div className='grid lg:grid-cols-12 2xl:gap-8 lg:gap-x-8 lg:gap-y-2 md:grid-cols-6 sm:grid-cols-6 max-sm:grid-cols-2 lg:pr-[134px] lg:pl-[134px] md:pr-[34px] md:pl-[34px] sm:pl-[34px] max-sm:pl-0 sm:pr-[34px] max-sm:pr-0  md:w-auto md:mx-0 sm:w-full sm:mx-auto max-sm:w-full max-sm:mx-auto  '>
         <div
           className="md:block sm:block max-sm:hidden
           absolute top-0 left-0 z-0
           border-t-[474px] border-l-[88vw] border-b-[176px]
           border-t-[#FFEC7A] border-l-[#FFEC7A] border-b-transparent
 
-          lg:border-l-[89vw] lg:border-b-[206px]
+          lg:border-l-[89vw] lg:border-b-[166px]
         "
         />
 
-        <div className='md:block z-100 sm:col-start-1 sm:col-span-5 max-sm:col-start-1 max-sm:col-span-2 leading-[1.5] md:w-[700px] lg:mt-10.5 lg:mb-16 md:mb-10.5 sm:mb-6.5 max-sm:mb-6.5'>
+        <div className='md:block z-100 lg:col-start-1 lg:col-span-5 max-sm:col-start-1 max-sm:col-span-2 leading-[1.5] md:w-[700px] lg:mt-10.5 lg:mb-16 md:mb-10.5 sm:mb-6.5 max-sm:mb-6.5'>
           <div className='text-[36px] font-bebas tracking-[0.15em]'>
             have any questions?
           </div>
@@ -29,6 +30,58 @@ const Questions = () => {
 
           <ContactForm />
         </div>
+
+        <div className='md:block z-100 lg:col-start-7 lg:col-span-4 max-sm:col-start-1 max-sm:col-span-2 leading-[1.5] md:w-[700px] lg:mt-20 lg:mb-16 md:mb-10.5 sm:mb-6.5 max-sm:mb-6.5'>
+          <div className='mb-6'>
+            <p className='text-[24px] font-sans font-medium text-[rgba(0,0,0,0.29)]'>
+              Our phone
+            </p>
+            <p className='text-[16px] font-sans'>
+              (270) 555-0117
+            </p>
+          </div>
+          <div className='mb-6'>
+            <p className='text-[24px] font-sans font-medium text-[rgba(0,0,0,0.29)]'>
+              Our email
+            </p>
+            <p className='text-[16px] font-sans'>
+              potrpots@gmail.com
+            </p>
+          </div>
+          <div className='mb-6'>
+            <p className='text-[24px] font-sans font-medium text-[rgba(0,0,0,0.29)]'>
+              Our office
+            </p>
+            <p className='text-[16px] font-sans'>
+              United Kingdom  99 Staple Hill Road,<br></br> Bristol, BS16 5AD
+            </p>
+          </div>
+
+        </div>
+
+        <div className='lg:col-start-11 lg:col-span-2 z-20'>
+          <div className='translate-x-[4rem] translate-y-[-5.5rem]'>
+            <Image
+              src="/images/Circle.svg"
+              alt="Circle"
+              width={161}
+              height={164}
+              className="object-contain w-[161px] h-[164px] z-20"
+            />
+          </div>
+        </div>
+
+        <div className='lg:col-start-1 lg:col-span-2 '>
+          <div className='mb-6 mt-30'>
+            <p className='text-[24px] font-bebas leading-[24px] tracking-[0.155em] mb-2.5 font-medium'>
+              Adrress
+            </p>
+            <p className='text-[16px] leading-[1.5] font-sans'>
+              United Kingdom <br></br> 99 Staple Hill Road,<br></br> Bristol, BS16 5AD
+            </p>
+          </div>
+        </div>
+
       </div>
     </section>
   )
--- src/app/components/SecondOrder.tsx
+++ src/app/components/SecondOrder.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SecondOrder = () => {
   return (
-    <section id='second_order' className='relative flex flex-col lg:min-h-auto md:min-h-auto 2xl:mb-10 md:mb-20 sm:mb-6 md:mt-0 sm:mt-25 max-sm:mt-25 max-sm:mb-10 lg:mt-0'>
+    <section id='second_order' className='relative flex flex-col lg:min-h-auto md:min-h-auto 2xl:mb-10 md:mb-0 sm:mb-6 md:mt-0 sm:mt-25 max-sm:mt-25 max-sm:mb-10 lg:mt-0'>
       <div className='grid lg:grid-cols-12 2xl:gap-8 lg:gap-x-4 lg:gap-y-2 md:gap-x-8 sm:gap-x-8 md:grid-cols-6 sm:grid-cols-6 max-sm:grid-cols-2 lg:mt-0 lg:pr-[134px] lg:pl-0 md:pr-[34px] md:pl-[34px] sm:pl-[34px] sm:pr-[34px] max-sm:pl-[34px] max-sm:pr-[34px] md:w-auto md:mx-0 sm:w-full sm:mx-auto max-sm:mx-auto max-sm:justify-center max-sm:w-full max-sm:px-4 '>
         <div className="lg:block md:hidden sm:hidden relative lg:col-start-1 lg:col-span-6 max-sm:hidden">
 
